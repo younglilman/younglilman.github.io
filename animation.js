5d7d730e322f48cbaaff8953ce577b138d2f1db9
lilman <dimanechaev9@gmail.com>
2021-05-02 19:58:42 +0700

    2.1

--- animation.js
+++
@@ -1,2 +0,0 @@
-let blocks = document.getElementsByClassName('courseProgram');
-    
--- scroll.js
+++ scroll.js
@@ -1,1 +1,17 @@
-
+$(function() {
+  var menuhe = $('#menu').height(); // высота меню
+  var menu = menuhe+20; // прибавляем 20
+  $("area[href*=#],a[href*=#]:not([href=#]):not([href^='#tab']):not([href^='#quicktab']):not([href^='#pane']):not([href *= '#inline'])").click(function() {
+  if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+    if (target.length) {
+      $('html,body').animate({
+        scrollTop: target.offset().top - menu
+        }, 800);
+      return false;
+    }
+  }
+});
+});
+  
